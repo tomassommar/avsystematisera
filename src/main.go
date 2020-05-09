8cedd4edceefabfd9638f4eefbec599415727520
Tomas Sommar <tomas.sommar@klarna.com>
2020-05-10 00:55:05 +0200

    Now with APK

--- src/main.go
+++ src/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/tomassommar/avsystematisera/repository"
+	"sort"
+	"strconv"
 )
 
 func main() {
-	stores := repository.GetStores()
-	for _, element := range stores {
-		fmt.Println(element.ID)
+	products := repository.GetProducts()
+	sort.Slice(products, func(i, j int) bool {
+		return products[i].Apk() < products[j].Apk()
+	})
+	for _, element := range products{
+		// fmt.Println(element.ID)
 		fmt.Println(element.Name)
-		fmt.Println(element.Street)
-		fmt.Println(element.City)
-		fmt.Println(element.PostalCode)
-	}
+		fmt.Println(element.Price)
+		fmt.Println(element.Volume)
 
+		fmt.Println(element.AlcoholPercentage)
+		fmt.Println("APK: " + strconv.FormatFloat(element.Apk(), 'E', -1, 64))
+	}
 }
--- src/repository/repository.go
+++ src/repository/repository.go
@@ -5,11 +5,17 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
-// GetStores returns store inventory
+// GetStores returns store storeInventory
 func GetStores() []Store {
-	return inventory.Inventory
+	return storeInventory.Inventory
+}
+
+func GetProducts() []Product{
+	return productInventory.Inventory
 }
 
 // Store contains ID, address and coordinates on RT90x format
@@ -25,17 +31,56 @@ type Store struct {
 	RT90y       string   `xmö:"RT90y"`
 }
 
+type AlcoholPercentage float64
+func(value *AlcoholPercentage) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error{
+	var s string
+	err := d.DecodeElement(&s, &start)
+	if err != nil {
+		return err
+	}
+	parsed, err := strconv.ParseFloat(strings.TrimRight(s, "%"), 64)
+	if err!=nil{
+		return err
+	}
+	*value = AlcoholPercentage(parsed)
+	return nil
+}
+
+type Product struct {
+	ID string `xml:"Artikelid"`
+	Name string `xml:"Namn"`
+	Price float64 `xml:"Prisinklmoms"`
+	Volume float64 `xml:"Volymiml"`
+	VolumeUnitPrice float64 `xml:"PrisPerLiter"`
+	AlcoholPercentage AlcoholPercentage `xml:"Alkoholhalt"`
+}
+
+func (product Product) Apk() float64{
+	alcoholVolume := product.Volume * (float64(product.AlcoholPercentage)/100)
+	return alcoholVolume/product.Price
+}
+
 type stores struct {
 	XMLName   xml.Name `xml:"ButikerOmbud"`
 	Inventory []Store  `xml:"ButikOmbud"`
 }
 
-var inventory stores
+type articles struct {
+	XMLName xml.Name `xml:"artiklar"`
+	Inventory []Product `xml:"artikel"`
+}
 
+var storeInventory stores
+var productInventory articles
 func init() {
 	pwd, _ := os.Getwd()
-	data, _ := ioutil.ReadFile(filepath.Join(pwd, "../resources/stores.xml"))
-	x := xml.Unmarshal(data, &inventory)
+	storeData, _ := ioutil.ReadFile(filepath.Join(pwd, "../resources/stores.xml"))
+	x := xml.Unmarshal(storeData, &storeInventory)
+	if x != nil {
+		panic(x)
+	}
+	productData, _ := ioutil.ReadFile(filepath.Join(pwd, "../resources/articles.xml"))
+	x = xml.Unmarshal(productData, &productInventory)
 	if x != nil {
 		panic(x)
 	}
